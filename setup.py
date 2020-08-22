6c6266be06aa3173dc7ed5da8d524dbdcef5ce44
StevenThomi <68867502+StevenThomi@users.noreply.github.com>
2020-08-22 18:34:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
+with open('README.me') as readme_file:
     readme = readme_file.read()
 
 with open('HISTORY.rst') as history_file:
@@ -18,7 +18,7 @@ test_requirements = ['pytest>=3', ]
 
 setup(
     author="Steve Thomi",
-    author_email='steveth437@gmail.com',
+    author_email='stevenkthomi@gmail.com',
     python_requires='>=3.5',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -31,23 +31,23 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    description="ECE Design Project",
+    description="Using a small single-board computer, develop an IoT application to generate a temperature and humidity mapping of a wide geometrical location.",
     entry_points={
         'console_scripts': [
-            'ece_design_project=ece_design_project.cli:main',
+            'eee3097s_project=eee3097s_project.cli:main',
         ],
     },
     install_requires=requirements,
     license="MIT license",
     long_description=readme + '\n\n' + history,
     include_package_data=True,
-    keywords='ece_design_project',
-    name='ece_design_project',
-    packages=find_packages(include=['ece_design_project', 'ece_design_project.*']),
+    keywords='eee3097s_project',
+    name='eee3097s_project',
+    packages=find_packages(include=['eee3097s_project', 'eee3097s_project.*']),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
-    url='https://github.com/StevenThomi/ece_design_project',
+    url='https://github.com/StevenThomi/eee3097s_project',
     version='0.1.0',
     zip_safe=False,
 )
