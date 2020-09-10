0dfb0f61e3b205a2f70848024d0b2ced3ad80039
StevenThomi <68867502+StevenThomi@users.noreply.github.com>
2020-09-10 21:35:37 +0300

    Update dh22Sensor.py

--- eee3097s_project/Command Line Scripts/dh22Sensor.py
+++ eee3097s_project/Command Line Scripts/dh22Sensor.py
@@ -18,36 +18,34 @@ class dh22Sensor(Sensor):
         super().__init__()
         self.__temperature = 0
         self.__humidity = 0
+        self.__alert = Alert(1)
+
 
 # simulate the temperature in a 0°C to 70°C range - using random numbers
     def getTemperature(self):
-        self.__temperature = random() * 70
+        self.__temperature = random() * 100
         return self.__temperature
-    
+
 # simulate the humidity in a 0% to 100% range - using random numbers
     def getHumidity(self):
         self.__humidity = random() * 100
         return self.__humidity
-    
+
 # extract meaning from simulated measurands through alerts - defined in a
 # table of alerts
     def getAlert(self):
         if(self.__temperature > 50 and self.__humidity < 30):
-            return Alert(4)
+            self.__alert = Alert(4)
         elif(self.__temperature > 50):
-            return Alert(2)
+            self.__alert = Alert(2)
         elif(self.__humidity < 30):
-            return Alert(3)
+            self.__alert = Alert(3)
         else:
-            return Alert(1)
+            self.__alert = Alert(1)
+        return self.__alert
 
 # print sensor output + dh22 sensor output
     def printSensor(self):
-        temperature = getTemperature()
-        humidity = getHumidity()
-        # writeToDatabase()
-        # write the fields to the linked database
-        
         print("DH22 Sensor: " + self.__str__() + "\tTemperature: " + \
-                str(temperature) + "°C\tHumidity: " + str(humidity) + \
-                "%\tAlert: " + str(getAlert()))
+                str(self.__temperature) + "°C\tHumidity: " + \
+                str(self.__humidity) + "%\tAlert: " + str(self.__alert))
