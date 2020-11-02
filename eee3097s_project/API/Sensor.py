6c5fee064bd9eb9213f99302e9c32df70689d609
StevenThomi <68867502+StevenThomi@users.noreply.github.com>
2020-11-02 11:54:52 +0300

    Update Sensor.py

--- eee3097s_project/API/Sensor.py
+++ eee3097s_project/API/Sensor.py
@@ -1,30 +1,30 @@
 # Sensor class
 class Sensor:
     def __init__(self,temperatureC=0,temperatureF=0,luminosity=0):
-        self.temperatureC = temperatureC
-        self.temperatureF = temperatureF
-        self.luminosity = luminosity
+        self.__temperatureC = temperatureC
+        self.__temperatureF = temperatureF
+        self.__luminosity = luminosity
 
     def setTemperatureInC(self, channel=0):
         # calculate temperature in degrees C
-        self.temperatureC = channel/1024
+        self.__temperatureC = channel/1024
 
     def setTemperatureInF(self, channel=0):
         # calculate temperature in degrees F
-        self.temperatureF = ((channel/1024)*(9/5))+32
+        self.__temperatureF = ((channel/1024)*(9/5))+32
 
     def setLuminosity(self, channel=0):
         # calculate luminosity as a percentage
-        self.luminosity = (channel-1850)/655
+        self.__luminosity = (channel-1850)/655
 
     def getTemperatureInC(self):
         # return temperature in C
-        return self.temperatureC
+        return self.__temperatureC
 
     def getTemperatureInF(self):
         # return temperature in F
-        return self.temperatureF
+        return self.__temperatureF
 
     def getLuminosity(self):
         # return luminosity as a percentage
-        return self.luminosity
+        return self.__luminosity
